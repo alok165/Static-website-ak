801b3776c8ca399347816df6d6559bd062a8cca8
alokaz400 <alok165@gmail.com>
2022-06-07 10:22:21 +1000

    fixed the merge issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -6,12 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-=======
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
->>>>>>> a107b192ff3e739859e3b09cc5ce87c139bb6a34
